f45dbf07b4830e84d1da2401b6ea6755babb50bb
Julian Rey <jrey@EliteBook.home-domain.com.ar>
2020-05-24 13:06:22 -0300

    Se agrega toda la funcionalidad para crear/recuperar js.

--- js/app.js
+++ js/app.js
@@ -2,14 +2,71 @@ storage = window.localStorage;
 //storage.clear();
 
 $(document).ready(function(){
-    $('#header-text, #payload-text, #signature-text').on('keypress',function(e) {
-      if(e.which == 13) {
+    $('#header-text, #payload-text, #signature-text').on('keyup',function() {
         jwt = create_JWT($.trim($('#header-text').val()), $.trim($('#payload-text').val()), $.trim($('#signature-text').val())) 
-        $('#JWT-text').val(jwt);
-        store( jwt );
-      }
+        if( jwt != 1 ) {
+            $('#JWT-text').val(jwt);
+        } else {
+            //cambiar color
+        }
     });
+    $('#clear').click(function() {
+        if( confirm('¿Quiere eliminar los JWT almacenados?') ){
+            storage.removeItem( 'arr' );
+            $("#historial").children().hide();
+        }
+    });
+    $('#save').click(function() {
+        jwt = $('#JWT-text').val();
+        store( jwt );
+        updateHistorial();
+    });
+    $('#help-p').click(function() {
+        $('#help-text').toggle();
+    });
+    $(".jwts").click(function() {
+        $(this).next().toggle();
+    })
+    $(".clave").on('keypress',function(e) {
+        if( e.which == 13 ) {
+            completar($(this).attr('id')[3],$(this).val());
+        }
+    });
+
 });
+
+function completar(position, secret ) {
+    arr = getObj('arr');
+    if( arr == undefined ) {
+
+    } else {
+        jwt = decode_JWT( arr[position] , secret );
+        if ( jwt[0] == 0 ) {
+        // El jwt era valido.
+            $("#header-text").val(JSON.stringify(jwt[1]));
+            $("#payload-text").val(JSON.stringify(jwt[2]));
+            $("#signature-text").val(JSON.stringify(secret));
+            $("#JWT-text").val( arr[position] );
+        }
+    }
+}
+
+function updateHistorial() {
+    if ( ( arr = getObj( 'arr' ) ) == undefined ) {
+        return;
+    }
+    div = $("#historial").children().first();
+    for( i=0 ; i < 5 ; i++) {
+        if ( arr[i] != undefined ) {
+            div.children().first().text( "JWT" + i+1 );
+            div.show();
+            div.children("div").hide()
+        } else {
+            div.hide();
+        }
+        div = div.next();
+    }
+}
 
 function store( nuevo ) {
     if( getObj( 'arr' ) == undefined ){
@@ -20,7 +77,6 @@ function store( nuevo ) {
         newarr = cascada( historial , nuevo , 0 );
         setObj( 'arr' , newarr );
     }
-    console.log(getObj( 'arr' ));
 }
 
 function cascada( arreglo , elemento , posicion ) {
@@ -52,3 +108,5 @@ function getObj( key ) {
       return undefined;
     }
 }
+
+updateHistorial();
