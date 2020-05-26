ae59f9d2ab27ac0f7eb3b6420b10fe862fbb7e7c
Julian Rey <jrey@EliteBook.home-domain.com.ar>
2020-05-26 19:55:57 -0300

    Add logo + fix clave + fixes varios

--- js/app.js
+++ js/app.js
@@ -2,7 +2,9 @@ storage = window.localStorage;
 //storage.clear();
 
 $(document).ready(function(){
+    checkDay();
     updateHistorial();
+    $("#help-text").hide();
     $('#header-text, #payload-text, #signature-text').on('keyup',function() {
         jwt = create_JWT($.trim($('#header-text').val()), $.trim($('#payload-text').val()), $.trim($('#signature-text').val())) 
         if( jwt != 1 ) {
@@ -25,7 +27,9 @@ $(document).ready(function(){
         $('#help-text').toggle();
     });
     $(".jwts").click(function() {
-        $(this).next().toggle();
+        console.log($(this));
+        $(this).hide();
+        $(this).next().show();
     })
     $(".clave").on('keypress',function(e) {
         if( e.which == 13 ) {
@@ -35,47 +39,73 @@ $(document).ready(function(){
     $(".dropdown-item").click(function() {
         completeHeader($(this).text());
     })
-    $("#day").click(function(){
-      alert("Todavia no funciono ale. LEAVE ME ALONEEEEEEE");
-    });
-    $("#night").click(function(){
-       alert("Todavia no funciono ale. LEAVE ME ALONEEEEEEE");
-    });
+    $("#day").click( function() { setDay( 'day' ) });
+    $("#night").click( function() {setDay( 'night' )});
 
 });
 
+function checkDay() {
+    console.log(storage['night']);
+    if( storage['night'] == undefined ) {
+        setDay( "day" );
+    } else {
+        setDay( "night" );
+    }
+}
+
+function setDay( day ) {
+    if( day === "day" ) {
+        $("body").removeClass('night-mode');
+        $("#day").addClass('active');
+        $("#night").removeClass('active');
+        storage.removeItem( 'night' )
+    } else {
+        $("body").addClass('night-mode');
+        $("#day").removeClass('active');
+        $("#night").addClass('active');
+        storage.setItem( 'night' , 'true' )
+    }
+
+}
+
+
+
 function guardar() {
     jwt = $('#JWT-text').val();
-    if ( jwt === create_JWT($.trim($('#header-text').val()), $.trim($('#payload-text').val()), $.trim($('#signature-text').val()))) {
-        id = $('#JWT-id').val();
-        alg = JSON.parse($.trim($('#header-text').val())).alg;
-        store( jwt , id , alg );
-        updateHistorial();
+    id = $('#JWT-id').val();
+    if ( id != '' ) {
+        if ( jwt === create_JWT($.trim($('#header-text').val()), $.trim($('#payload-text').val()), $.trim($('#signature-text').val()))) {
+            alg = JSON.parse($.trim($('#header-text').val())).alg;
+            store( jwt , id , alg );
+            updateHistorial();
+        } else {
+            alert( "El JWT no coincide con los campos ingresados.");
+        }
     } else {
-        //FIXME Mensaje error
+        alert( "Debe ingresar un identificador." );
     }
 }
 
 function completeHeader(alg) {
-    $("#header-text").val('{\n\t"alg": "' + alg + '",\n\t "typ": "JWT"\n}');
+    $("#header-text").val('{\n\t"alg": "' + alg + '",\n\t"typ": "JWT"\n}');
 }
 
-//FIXME
 function completar(position, secret ) {
     arr = getObj('arr');
-    if( arr == undefined ) {
-
-    } else {
+    if( arr != undefined ) {
         jwt = decode_JWT( arr[position][0] , secret );
         console.log(secret);
         if ( jwt[0] == 0 ) {
         // El jwt era valido.
             $("#header-text").val(JSON.stringify(jwt[1]));
             $("#payload-text").val(JSON.stringify(jwt[2]));
-            $("#signature-text").val(JSON.stringify(secret));
-            $("#JWT-text").val( arr[position] );
+            $("#signature-text").val(JSON.stringify(secret).replace(/"/g, '' ));
+            $("#JWT-text").val( arr[position][0] );
+        } else {
+            alert("Clave incorrecta");
         }
     }
+    updateHistorial();
 }
 
 function updateHistorial() {
@@ -91,12 +121,14 @@ function updateHistorial() {
         if ( arr[i] != undefined ) {
             div.children().first().text( arr[i][1] );
             div.show();
-            div.children("div").hide()
+            div.children("p").show()
+            div.children("input").hide()
         } else {
             div.hide();
         }
         div = div.next();
     }
+    $(".clave").val("");
 }
 
 function store( jwt , id , alg ) {
